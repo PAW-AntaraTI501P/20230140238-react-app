bab0689a87f36b17b4744a55e179f328dd507283
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2025-07-25 09:45:10 +0700

    react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import react, {useEffect, useState} from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(()=> {
+    fetch('http://localhost:3001')
+    .then(response => response.json())
+    .then(data=> setMessage(data.message))
+    .catch(error => console.error('Error:', error));}, []);
+
+  return (
+    <div>
+      <h1>Integrasi react dan node.js</h1>
+      <p>Pesan dari server: {message}</p>
+    </div>
+  );
+}
+
+export default App;
